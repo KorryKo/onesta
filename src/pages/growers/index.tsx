0b76062c1d4bf67425014f4caf17c2535a09aee3
Marina <maryaleks@mail.ru>
2023-04-08 12:15:17 +0300

    bring growers data

--- src/pages/growers/index.tsx
+++ src/pages/growers/index.tsx
@@ -1,8 +1,41 @@
 import Breadcrumbs from "@/components/breadcrumbs";
+import DataTable from "@/components/data-table";
 import Layout from "@/components/layout";
 import { MenuItems } from "@/global/types";
+import { GetServerSideProps } from "next";
 
-const Growers: React.FC = () => {
+interface GrowersPageProps {
+  growers: Grower[];
+}
+
+interface Grower {
+  id: string;
+  name: string;
+  lastName: string;
+  email: string;
+  farms: Farms[];
+}
+
+interface Farms {
+  id: string;
+  name: string;
+  address: string;
+}
+
+interface GrowersData {
+  growers: Grower[];
+  count: number;
+}
+
+export const getServerSideProps: GetServerSideProps<
+  GrowersPageProps
+> = async (): Promise<{ props: GrowersPageProps }> => {
+  const res = await fetch("https://testapi.onesta.farm/v1/growers");
+  const data: GrowersData = await res.json();
+  return { props: { growers: data.growers } };
+};
+
+const Growers: React.FC<GrowersPageProps> = ({ growers }) => {
   const growersBradcrumbs: MenuItems[] = [
     { title: "Inicio", path: "/" },
     { title: "Agricultores", path: "/growers" },
@@ -11,7 +44,9 @@ const Growers: React.FC = () => {
   return (
     <Layout>
       <Breadcrumbs items={growersBradcrumbs} />
-      <h1>Growers</h1>
+      <h1 className="font-normal text-2xl my-6">Growers</h1>
+      <hr className="border border-lightGray2" />
+      <DataTable data={growers} />
     </Layout>
   );
 };
