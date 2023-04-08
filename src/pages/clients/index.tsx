b5d5b16615cac03e153513a1e8484c275e76f69a
Marina <maryaleks@mail.ru>
2023-04-08 12:32:23 +0300

    bring clients data

--- src/pages/clients/index.tsx
+++ src/pages/clients/index.tsx
@@ -1,8 +1,34 @@
 import Breadcrumbs from "@/components/breadcrumbs";
+import DataTable from "@/components/data-table";
 import Layout from "@/components/layout";
 import { MenuItems } from "@/global/types";
+import { GetServerSideProps } from "next";
 
-const Clients: React.FC = () => {
+interface ClientsPageProps {
+  clients: Client[];
+}
+
+interface Client {
+  id: string;
+  name: string;
+  lastName: string;
+  email: string;
+}
+
+interface ClientsData {
+  clients: Client[];
+  count: number;
+}
+
+export const getServerSideProps: GetServerSideProps<
+  ClientsPageProps
+> = async (): Promise<{ props: ClientsPageProps }> => {
+  const res = await fetch("https://testapi.onesta.farm/v1/clients");
+  const data: ClientsData = await res.json();
+  return { props: { clients: data.clients } };
+};
+
+const Clients: React.FC<ClientsPageProps> = ({ clients }) => {
   const clientsBradcrumbs: MenuItems[] = [
     { title: "Inicio", path: "/" },
     { title: "Clientes", path: "/clients" },
@@ -11,7 +37,9 @@ const Clients: React.FC = () => {
   return (
     <Layout>
       <Breadcrumbs items={clientsBradcrumbs} />
-      <h1>Clients</h1>
+      <h1 className="font-normal text-2xl my-6">Clients</h1>
+      <hr className="border border-lightGray2" />
+      <DataTable data={clients} />
     </Layout>
   );
 };
